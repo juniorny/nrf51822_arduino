26320820bddebb4b6d63f650a6074012ee997ff3
zhong <455509712@qq.com>
2018-12-05 10:57:23 +0800

    add myled example

---
+++ hardware/nRF51822/1.0.8/libraries/MyLed/examples/myled/myled.ino
@@ -0,0 +1,23 @@
+/*
+  Copyright (c) 2018 zhong 
+*/
+#define BLE400
+#include <led.h>
+
+void setup() 
+{
+    B_LED0.on();
+    B_LED1.off();
+    B_LED2 << HIGH;
+    B_LED3.flash(500, 200, 5);
+
+}
+
+
+void loop() 
+{
+    B_LED4.fade();
+}
+
+
+
